b0972f3c55490670e272433fc80e76e32d30b5f9
Bhavish051 <53845440+Bhavish051@users.noreply.github.com>
2021-07-11 17:04:08 +1000

    Intial Commit

---
+++ src/main/java/com/MantraBazaar/Mantra_Bazaar/MantraBazaarApplication.java
@@ -0,0 +1,13 @@
+package com.MantraBazaar.Mantra_Bazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantraBazaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MantraBazaarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MantraBazaar/Mantra_Bazaar/MantraBazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MantraBazaar.Mantra_Bazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MantraBazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
